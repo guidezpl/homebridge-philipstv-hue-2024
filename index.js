2f02e3bbb05748055255f8336f1063692cc92058
bmccruz <bmccruz@gmail.com>
2021-01-04 21:23:32 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1296,7 +1296,7 @@ HttpStatusAccessory.prototype = {
 			this.enabled_services.push(this.ambilightService);
 			
 			if (this.has_hue) {
-			    this.hueService = new Service.Lightbulb(this.name + " Hue", '0e');
+			    this.hueService = new Service.Switch(this.name + " Hue", '0f');
 			
 				this.hueService
 					.getCharacteristic(Characteristic.On)
