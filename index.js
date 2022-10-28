dc723bc0c6a61f7b0532125cdd1ad8403efc98b1
Rafael Gaspar <rafael.gaspar@wooga.net>
2022-10-28 23:35:18 +0200

    Arghhh, more debug

--- index.js
+++ index.js
@@ -783,9 +783,10 @@ HttpStatusAccessory.prototype = {
 					var responseBodyParsed;
 					try {
 						responseBodyParsed = JSON.parse(responseBody);
+						that.log('%s - got answer %s %s %s', fctname, length(responseBodyParsed.values), responseBodyParsed.values[0], responseBodyParsed.values[1]);
 						if (responseBodyParsed && responseBodyParsed.values[0].value.data) {
 							tResp = responseBodyParsed.values[0].value.data.value;
-							that.log('%s - got answer %i %s', fctname, length(responseBodyParsed.values), responseBodyParsed.values[0]);
+							that.log('%s - got answer %s %s', fctname, length(responseBodyParsed.values), responseBodyParsed.values[0]);
 						} else {
 							that.log("%s - Could not parse message: '%s', not updating state", fctname, responseBody);
 						}
